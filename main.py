f697ad26348ccb3ed97b89ddd98b410e429214db
Adam26062808 <155439374+Adam26062808@users.noreply.github.com>
2024-10-08 11:30:44 +0000

    Finish the program (Do not run if you run you gay)

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
+from flask import Flask, request, render_template
+import random
 
+WORDS = ["apple", "grade", "grape", "melon"]
+
+secret_word = random.choice(WORDS).upper()
+
+app = Flask(__name__)
+
+@app.route("/", method = ["GET", "POST"])
+def index():
+    guess = [''] * len(secret_word)
+
+    if request.method == 'POST':
+        guess = [
+            request.form.get(f'letter(i+1)', '')
+            for i in range(len(secret_word))
+        ]
+
+        guess_word = ''.join(guess)
+
+        if len(guess_word) != len(secret_word):
+            return render_template('index.html', error="Word must be the correct length.", word_length = len(secret_word), guess = guess)
+
+
+        feedback = check_guess(guess_word, secret_word)
+
+        if guess_word == secret_word:
+            return render_template("index.html", feedback=feedback, succes=True, world_length = len(secret_word), guess = guess)
+        
+        return render_template("index.html", feedback = feedback, guess = guess, word_length = len (secret_word))
+    return render_template("index.html", feedback = feedback, guess = guess, word_length = len (secret_word))
+
+def check_guess(guess_word, secret_word):
+    feedback = []
+    for i in range(len(secret_word)):
+        if guess_word[i] == secret_word[i]:
+            feedback.append("Match")
+        elif guess_word[i] in secret_word:
+            feedback.append("Wrong position")
+        else:
+            feedback.append("Wrong")
+
+    return feedback
+
+if __name__ == "__main__":
+    app.run(debug = True)
