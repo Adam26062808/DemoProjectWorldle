f13ba781cd60a07ce250b9d1484c4c66642be823
Adam26062808 <155439374+Adam26062808@users.noreply.github.com>
2024-11-19 11:35:22 +0000

    UPDATE

---
+++ database.py
@@ -0,0 +1,117 @@
+import sqlite3
+
+db_name = 'wordle.db'
+conn = None
+cursor = None
+
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+def close():
+    cursor.close()
+    conn.close()
+
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+
+def make_table():
+    open()
+    do('''PRAGMA foreign_keys=on''')
+
+    do('''CREATE TABLE IF NOT EXISTS wordle (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       words VARCHAR)''')
+    
+    do('''CREATE TABLE IF NOT EXISTS leaderboard ( 
+       id INTEGER PRIMARY KEY AUTOINCREMENT, 
+       name VARCHAR, 
+       attempts INTEGER, 
+       word_id INTEGER, 
+       FOREIGN KEY (word_id) REFERENCES wordle (id))''')
+    close()
+
+def add_words():
+    open()
+    words = [('hamburger',),
+             ('pizza',),
+             ('audio',),
+             ('playstation',),
+             ('silly',),
+             ('cook',),
+             ('laptop',),
+             ('screen',),
+             ('account',),
+             ('Table',),
+             ('uniform',),
+             ('unique',),
+             ('galaxy',),
+             ('Earth',),
+             ('smartphone',),
+             ('image',),
+             ('mouse',),
+             ('shark',),
+             ('diamond',),
+             ('dream',),
+             ('imagine',),
+             ('smile',),
+             ('celebration',),
+             ('kindness',),
+             ('love',),
+             ('applause',),
+             ('balloon',),
+             ('sunshine',),
+             ('moonlight',),
+             ('adventure',),
+             ('explore',),
+             ('incredible',),
+             ('raspberry',),
+             ('pineapple',),
+             ('Cherry',),
+             ('Snake',),
+             ('Sugar',),
+             ('running',),
+             ('keyboard',),
+             ('chocolate',),
+             ('line',),
+             ('basketball',),
+             ('soccer',),
+             ('arrow',),
+             ('water',),
+             ('lava',),
+             ('gold',),
+             ('iron',),
+             ('crystal',),
+             ('stone',),
+             ('dirt',)]
+        
+    cursor.executemany('INSERT INTO wordle (words) VALUES (?)', words)
+    conn.commit()
+    close()
+
+def get_words():
+    open()
+    cursor.execute('SELECT * FROM wordle')
+    wordle_data = cursor.fetchall()
+    close()
+    return wordle_data
+
+
+def get_player():
+    open()
+    cursor.execute('SELECT * FROM leaderboard')
+    leaderboard_data = cursor.fetchall()
+    close()
+    return leaderboard_data
+
+def make_datebase():
+    make_table()
+    add_words()
+
+def add_player(name, attemps, word_id):
+    open()
+    cursor.execute("INSERT INTO leaderboard (name, attempts, word_id) VALUES (?,?,?)", (name, attemps, word_id))
+    print(f"The player: {name}, takes {attemps} attemps to finish this {word_id} word")
+    close()
--- main.py
+++ main.py
@@ -1,47 +1,68 @@
 from flask import Flask, request, render_template
-import random
+from random import choice
+import database
+import os
 
-WORDS = ["apple", "grade", "grape"2, "melon"]
 
-secret_word = choice(WORDS).upper()
+if not os.path.exists("wordle.db"):
+    database.make_datebase()
+WORDS = database.get_words()
+print(WORDS)
+
+
+def choose_word():
+    global WORDS
+    choosen_word = choice(WORDS) 
+    word_id = choosen_word[0]
+    secret_word = choosen_word[1]
+    return word_id, secret_word
+
+
 
 app = Flask(__name__)
 
-@app.route("/", method = ["GET", "POST"])
+@app.route("/")
 def index():
-    guess = [''] * len(secret_word)
+    return render_template("index.html")
 
-    if request.method == 'POST':
-        guess = [
-            request.form.get(f'letter(i+1)', '')
-            for i in range(len(secret_word))
-        ]
+word_id = None
+@app.route("/wordle_page", methods = ["GET", "POST"])
+def wordle_page():
+    global word_id
+    attempts = 0
+    if not word_id:
+        word_id, secret_word = choose_word()
+    guess = [""] * len(secret_word)
+    if request.method == "POST":
+        name = request.form.get("name_input").upper()
+        guess = [request.form.get(f"letter{i+1}", "").upper()
+                 for i in range(len(secret_word))]
+        guess_word = "".join(guess)
 
-        guess_word = ''.join(guess)
+        if request.form.get("submit"):
+            attempts +=  1
 
         if len(guess_word) != len(secret_word):
-            return render_template('index.html', error="Word must be the correct length.", word_length = len(secret_word), guess = guess)
-
-
+            return render_template("wordle_page.html", error = "Not Enough Word", word_length = len(secret_word), guess = guess)
         feedback = check_guess(guess_word, secret_word)
 
         if guess_word == secret_word:
-            return render_template("index.html", feedback=feedback, succes=True, world_length = len(secret_word), guess = guess)
+            database.add_player(name, attempts, word_id)
+            word_id = None
+            return render_template("wordle_page.html", success = True, feedback = feedback, word_length = len(secret_word), guess = guess)
         
-        return render_template("index.html", feedback = feedback, guess = guess, word_length = len (secret_word))
-    return render_template("index.html", feedback = feedback, guess = guess, word_length = len (secret_word))
+        return render_template("wordle_page.html", feedback = feedback, word_length = len(secret_word), guess = guess)
+    return render_template("wordle_page.html",word_length = len(secret_word), guess = guess)
 
 def check_guess(guess_word, secret_word):
     feedback = []
-    for i in range(len(secret_word)):
+    for i in range(len(guess_word)):
         if guess_word[i] == secret_word[i]:
-            feedback.append("Match")
+            feedback.append("Correct!")
         elif guess_word[i] in secret_word:
-            feedback.append("Wrong position")
+            feedback.append("Wrong Position!")
         else:
-            feedback.append("Wrong")
-
+            feedback.append("Incorrect!")
     return feedback
-
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
